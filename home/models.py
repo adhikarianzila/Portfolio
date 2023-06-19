4df7a46664fe685253276b0382ee7bf8f4bae3d7
adhikarianzila <adhikari.anzila@gmail.com>
2023-06-19 17:07:09 +0545

    first commit

---
+++ home/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=50)
+    subject = models.TextField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
+
+class ContactInformation(models.Model):
+    address1 = models.CharField(max_length=500)
+    address2 = models.CharField(max_length=500)
+    phone_no = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    time = models.CharField(max_length=50)
+    def __str__(self):
+        return self.address1
+    
+class Services(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.CharField(max_length=100)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+    
+class Feedback(models.Model):
+    name = models.CharField(max_length=300)
+    post = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    comment = models.TextField()
+    def __str__(self):
+        return self.post
